033b73437b5df3fab230e2b46d6fd6ce11df4dbc
madokakmr <131327262+madokakmr@users.noreply.github.com>
2024-07-26 14:50:04 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript
+//console.log('Hello world!');
+
+$(function(){
+    $('.bxslider').bxSlider({
+      // オプションを書く場所
+      mode: 'fade',
+      captions: true,
+      auto: true,
+      speed: 500,
+      pause: 2500,
+      autoHover: false,
+      pager: false,
+    });
+  });
